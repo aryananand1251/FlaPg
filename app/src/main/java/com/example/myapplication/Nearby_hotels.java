776fc6d7a4b92c5a42fa7b5f57b5ffe085cf3feb
aryan <aryananand12517@gmail.com>
2024-03-26 23:00:48 +0530

    aaded java file of near college,office,hotels

---
+++ app/src/main/java/com/example/myapplication/Nearby_hotels.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Nearby_hotels extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nearby_hotels);
+    }
+}
